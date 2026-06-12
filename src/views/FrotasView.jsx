b10ab91048d59e1c1e6ce3d01a37df310feca385
DieguinhoSoares <diegosoares@hotmail.fr>
2026-06-11 22:45:13 -0300

    Update FrotasView.jsx

--- src/views/FrotasView.jsx
+++ src/views/FrotasView.jsx
@@ -1006,7 +1006,7 @@ function ValidacoesTab({ cards, onValidar, onRejeitar }) {
             {modal.card.conclusaoFoto && (
   <div style={{ marginBottom:12 }}>
     <div style={{ color:T.textMuted, fontSize:9, fontFamily:FONT, marginBottom:4, textTransform:'uppercase', letterSpacing:'0.07em' }}>📷 Foto de conclusão</div>
-    <img src={modal.card.conclusaoFoto} alt="conclusao" style={{ width:'100%', borderRadius:T.r, maxHeight:180, objectFit:'cover' }}/>
+    <img src={modal.card.conclusaoFoto} alt="conclusao" style={{ width:'100%', borderRadius:T.r, maxHeight:300, objectFit:'contain', background:T.surfaceAlt }}
   </div>
 )}
 {modal.card.conclusaoNota && (
@@ -1084,7 +1084,7 @@ function ValidacoesTab({ cards, onValidar, onRejeitar }) {
              {c.conclusaoFoto && (
   <div style={{ marginBottom:10 }}>
     <div style={{ color:T.textMuted, fontSize:9, fontFamily:FONT, marginBottom:4, textTransform:'uppercase', letterSpacing:'0.07em' }}>📷 Foto de conclusão</div>
-    <img src={c.conclusaoFoto} alt="conclusao" style={{ width:'100%', borderRadius:T.r, maxHeight:160, objectFit:'cover' }}/>
+    <img src={c.conclusaoFoto} alt="conclusao" style={{ width:'100%', borderRadius:T.r, maxHeight:280, objectFit:'contain', background:T.surfaceAlt }}
   </div>
 )}
 {c.interrupcaoDescricao && (
