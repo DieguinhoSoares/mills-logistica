cc50a2464fcf5d3ef440ac45138e7dfe896c4639
DieguinhoSoares <diegosoares@hotmail.fr>
2026-06-14 00:14:17 -0300

    Update FrotasView.jsx

--- src/views/FrotasView.jsx
+++ src/views/FrotasView.jsx
@@ -9,7 +9,6 @@ import Papa from 'papaparse'
 import { FreteEstimativa } from '../components/FreteEstimativa'
 import { RequestForm }     from '../components/RequestForm'
 import { collection, addDoc, updateDoc, doc, serverTimestamp } from 'firebase/firestore'
-import { FreteEstimativa } from '../components/FreteEstimativa'
 import { RequestForm }     from '../components/RequestForm'
 
 /* ══ SUBTYPE SELECT ══════════════════════════════════════════════════════════ */
